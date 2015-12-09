3dc22345a4d40c48db61a54eb82a78a2a537c624
Connor Kendrick <connorkendrickmusic@gmail.com>
2015-12-09 17:18:15 -0500

    Beginning to merge

--- listenersClub.py
+++ listenersClub.py
@@ -11,6 +11,7 @@ USER_AGENT = ""
 OAUTH_CONF_FILE = "./config/oauth.ini"
 
 class Bot:
+    # clear
     def __init__(self, user_agent, user_name):
         self.user_name = user_name
         self.reddit = praw.Reddit(user_agent)
@@ -20,15 +21,15 @@ class Bot:
             self.load_data()
         else:
             self.data = Data()
-
+    # clear
     def save_data(self):
         with open(STATE_DATA, 'wb') as output_file:
             pickle.dump(self.data, output_file, pickle.HIGHEST_PROTOCOL)
-
+    # clear
     def load_data(self):
         with open(STATE_DATA, 'rb') as input_file:
             self.data = pickle.load(input_file)
-
+    # clear
     def check_messages(self):
         messages = self.reddit.get_unread(limit=None)
         for msg in reversed(list(messages)):
@@ -36,15 +37,15 @@ class Bot:
             print(response)
             msg.reply(response)
             msg.mark_as_read()
-
+    # involved
     def check_events(self):
         for event in self.data.events:
             if not event.run_today:
-                if time.strftime("%A") == event.album_day:
-                    self._post_album()
-                    if time.strftime("%A") != event.analysis_day:
-                        event.run_today = True
-                if time.strftime("%A") == event.analysis_day:
+                if time.strftime("%A") == event.album_day: ####
+                    self._post_album() 
+                    if time.strftime("%A") != event.analysis_day: # delete
+                        event.run_today = True #?
+                if time.strftime("%A") == event.analysis_day: # merge
                     success = self._post_analysis()
                     if success:
                         if event.post_count != 0:
@@ -55,7 +56,7 @@ class Bot:
                     event.run_today = True
             elif time.strftime("%A") != event.album_day and time.strftime("%A") != event.analysis_day and event.run_today:
                 event.run_today = False
-
+    # clear
     def _authenticate_user(self, name, level):
         if level == 'Mod':
             mod_list = self.reddit.get_subreddit(SUBREDDIT).get_moderators()
@@ -70,12 +71,12 @@ class Bot:
             return False
         else:
             return False
-
+    # involved
     def _post_album_to_reddit(self, album):
         post_body = self._generate_post_body(album)
         print(post_body)
         self.reddit.submit(SUBREDDIT, "Week "+ str(self.data.week) + ": " + album.artist + " = " + album.album_title, text=str(post_body), send_replies=False)
-
+    # involved
     def _generate_post_body(self, album):
         post_body = "This Weeks Album Has Been Picked By /u/" + self.data.user_list[self.data.user_index].name
         post_body += "\n\n## ["+ album.artist +" - "+ album.album_title + "]("+album.link1+")\n\n### Details and Synopsis\n\n"
@@ -89,7 +90,7 @@ class Bot:
 
         post_body += "\n\n### Selection Reason\n\n" + album.selection_reason
         return post_body
-
+    # involved
     def _post_album(self):
         if self.data.user_index == len(self.data.user_list):
             self.data.user_index = 0
@@ -118,17 +119,17 @@ class Bot:
                 self.data.user_index += 1
                 if self.data.user_index == len(self.data.user_list):
                     self.data.user_index = 0
-
+    # involved
     def _post_analysis(self):
-        if len(self.data.user_list[self.data.user_index].submissions) > 0:
-            album = self.data.user_list[self.data.user_index].submissions[0]
+        if len(self.data.user_list[self.data.user_index].submissions) > 0: #remove
+            album = self.data.user_list[self.data.user_index].submissions[0] #remove
             if not album.posted:
                 return False
             post_body = "This Weeks Album Is '" + album.artist + " - " + album.album_title + "'  Picked By /u/" + self.data.user_list[self.data.user_index].name
             post_body += "\n\n### Analysis Questions\n\n" + album.analysis_questions
             self.reddit.submit(SUBREDDIT, "Week "+ str(self.data.week) + ": " + album.artist + " - " + album.album_title +" [ANALYSIS THREAD]", text=str(post_body), send_replies=False)
             print(album.analysis_questions)
-            self.data.week += 1
+            self.data.week += 1 # move to _post_album?
             print(str(len(self.data.user_list[self.data.user_index].submissions)))
             self.data.user_list[self.data.user_index].submissions.pop(0)
             print(str(len(self.data.user_list[self.data.user_index].submissions)))
@@ -136,7 +137,7 @@ class Bot:
         else:
             return False
         return True
-
+    # involved
     def _parse_command(self, msg):
         cmd = msg.subject
         args = msg.body
@@ -185,24 +186,24 @@ class Bot:
             return "Your Command has been processed."
         else:
             return success
-
+    # clear
     def _add_user(self, user_name):
         for user in self.data.user_list:
             if user.name == user_name:
                 return "Error: User Already Added!"
         self.data.user_list.append(User(user_name))
         return True
-
-    def _add_event(self, user_name, album_day, analysis_day, post_count, event_post_type):
+    # involved - cleared
+    def _add_event(self, user_name, post_day, post_count, event_post_type):
         for user in self.data.user_list:
             if user.name == user_name:
                 for event in self.data.events:
-                    if event.analysis_day == analysis_day and event.album_day == album_day:
+                    if event.post_day == post_day:
                         return "Error: Event already Added"
-                self.data.events.append(Event(album_day, analysis_day, post_count, event_post_type))
+                self.data.events.append(Event(post_day, post_count, event_post_type))
                 return True
         return "Error: User Name Not Recognised!"
-
+    # clear
     def _add_album(self, user_name, args):
         #TODO: verify no one has added album
         for user in self.data.user_list:
@@ -210,30 +211,31 @@ class Bot:
             if user.name == user_name:
                 return user.add_submission(args)
         return "Error: User Name Not Recognised!"
-
+    # clear
     def _get_user_list(self):
         if len(self.data.user_list) != 0:
             return self.data.user_list
         else:
             return "Error: No Users Added!"
-
+# clear
 class Data:
     def __init__(self):
         self.week = 0
         self.user_index = 0
         self.user_list = []
         self.events = []
-
+# involved
 class Event:
     eventpost_count = 0
     def __init__(self, album_day, analysis_day, post_count, post_type):
-        self.album_day = album_day
-        self.analysis_day = analysis_day
+        #self.album_day = album_day
+        #self.analysis_day = analysis_day
+        self.post_day = post_day # combine of album_day and analysis_day
         self.post_count = post_count
         self.post_type = post_type #list or username
         self.run_today = False
         Event.eventpost_count += 1
-
+# clear
 class User:
     usrpost_count = 0
 
@@ -250,7 +252,7 @@ class User:
                 return "Submission already added!"
         self.submissions.append(Submission(new_album))
         return True
-
+# involved
 class Submission:
     def __init__(self, args):
         self.posted = False
@@ -262,7 +264,7 @@ class Submission:
         self.label = args[5]
         self.description = args[6]
         self.selection_reason = args[7]
-        self.analysis_questions = args[8]
+        self.analysis_questions = args[8] # will be moved to the end of post album
         self.link1 = args[9]
         if len(args) == 11:
             self.link2 = args[10]
